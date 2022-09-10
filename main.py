a1663b95e3281069ec593aa79b7ff1f7a036711b
jpmasters <jonmasters72@gmail.com>
2022-09-10 16:05:21 +0100

    GPU mem growth

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 import pathlib
 import tensorflow as tf
-
 import time
-
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
-
 import numpy as np
 from PIL import Image
-
 import matplotlib.pyplot as plt
 
+
+# enable GPU dynamic memory allocation
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
